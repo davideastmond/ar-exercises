bb580823ff1df78ab9a246f31ffc16b55f090048
David Eastmond <davebuck1981@gmail.com>
2019-06-11 20:03:52 +0000

    completed Ex 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,3 +7,5 @@ puts "----------"
 Store.create(:name => 'Burnaby', :annual_revenue => 300000, :womens_apparel=> true, :mens_apparel => true)
 Store.create(:name => 'Richmond', :annual_revenue => 1260000, :womens_apparel=> true, :mens_apparel => false)
 Store.create(:name => 'Gastown', :annual_revenue => 190000, :womens_apparel=> false, :mens_apparel => true)
+
+puts Store.count
