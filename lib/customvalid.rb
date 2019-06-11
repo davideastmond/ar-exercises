4bba734b8afb36ba4eb705c581dbd68c584ddd2a
David Eastmond <davebuck1981@gmail.com>
2019-06-11 22:18:36 +0000

    completed custom validator

--- lib/customvalid.rb
+++ lib/customvalid.rb
@@ -1,3 +1,7 @@
 class MyValidator < ActiveModel::Validator
-
+  def validate(record)
+    if record.mens_apparel == false && record.ladies_apparel == false
+      record.errors[:mens_apparel] << 'Store must carry either mens apparel, womens or BOTH'
+    end
+  end
 end
